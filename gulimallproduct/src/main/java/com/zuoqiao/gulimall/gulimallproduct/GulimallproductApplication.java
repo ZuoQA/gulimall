4360dbc8d3776f772797deae009f11e0644ed930
QiAo Zuo <zqanju@163.com>
2021-06-29 13:01:51 +0800

    项目初始结构创建

---
+++ gulimallproduct/src/main/java/com/zuoqiao/gulimall/gulimallproduct/GulimallproductApplication.java
@@ -0,0 +1,13 @@
+package com.zuoqiao.gulimall.gulimallproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallproductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallproductApplication.class, args);
+    }
+
+}
